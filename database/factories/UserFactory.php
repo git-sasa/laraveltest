0001cb1e3dbfc6898ef3498aee940e8675fd03d8
杨飒 <1491937681@qq.com>
2023-10-09 09:49:31 +0800

    first commit

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,7 +4,6 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
-
 class UserFactory extends Factory
 {
     /**
@@ -12,6 +11,7 @@ class UserFactory extends Factory
      *
      * @return array
      */
+
     public function definition()
     {
         return [
@@ -21,6 +21,7 @@ class UserFactory extends Factory
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
         ];
+
     }
 
     /**
